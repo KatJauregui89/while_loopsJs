3c69a960218eec50d7fee01e2599629ea2c06948
katjauregui89 <katiajauregui@gmail.com>
2022-06-30 07:54:58 -0500

    Files

---
+++ app.js
@@ -0,0 +1,20 @@
+// let count = 0;
+// while (count < 10) {
+//     count++;
+//     console.log(count)
+// }
+
+// const secret = "BabyHippo";
+// let guess = prompt("Enter the secret code:");
+// while (guess !== secret) {
+//     guess = prompt("Enter the secret code:");
+// }
+// console.log("Congrats! You know the secret")
+
+// let input = prompt('Hey, say something')
+// while (true) {
+//     input = prompt(input);
+//     if (input.toLowerCase() === "stop") break;
+// }
+// console.log("You win")
+
